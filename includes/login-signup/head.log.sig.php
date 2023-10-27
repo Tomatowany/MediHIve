9d3c32ec0b6fa646dc624ca299ef41cb748796d3
Angelo John <146217744+Tomatowany@users.noreply.github.com>
2023-10-28 01:39:07 +0800

    update signup

--- includes/login-signup/head.log.sig.php
+++ includes/login-signup/head.log.sig.php
@@ -6,6 +6,6 @@
     <meta name="appleid-signin-redirect-uri" content="[REDIRECT_URI]">
     <meta name="appleid-signin-state" content="[STATE]">
     <link rel="stylesheet" href="vendor/bootstrap-5.0.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/log.sign.css?v=<?php echo time();?>">
+    <link rel="stylesheet" href="css/login.css?v=<?php echo time();?>">
     <title>MediHive</title>
 </head>
