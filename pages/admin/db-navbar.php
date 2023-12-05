bb650e84fb371224681b5dfd8ccd905ce038cdf2
Angelo John <146217744+Tomatowany@users.noreply.github.com>
2023-12-05 14:34:33 +0800

    margin status acc

--- pages/admin/db-navbar.php
+++ pages/admin/db-navbar.php
@@ -10,46 +10,44 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-            <ul class="navbar-nav me-3 mx-auto  mb-lg-0">
+            <ul class="navbar-nav me-5 mx-auto  mb-lg-0">
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
                         aria-expanded="false">
                         <i class="bi bi-person-badge"></i>
                         <?php
-                            
-                            if(isset($_SESSION['data'])){
-                                echo $_SESSION['data']['role']." ".$_SESSION['data']['firstName'];
-                            }
+
+                        if (isset($_SESSION['data'])) {
+                            echo $_SESSION['data']['role'] . " " . $_SESSION['data']['firstName'];
+                        }
                         ?>
                     </a>
                     <ul class="dropdown-menu dropdown-menu-end " aria-labelledby="navbarDropdown">
                         <?php
-                            if(isset($_SESSION['data'])){
-                                if($_SESSION['data']['role'] == "Staff"){
-                                    echo '<li><a class=" dropdown-item" href="#">Account Settings</a>';
-                                    echo '<li>
+                        if (isset($_SESSION['data'])) {
+                            if ($_SESSION['data']['role'] == "Staff") {
+                                echo '<li><a class=" dropdown-item" href="#">Account Settings</a>';
+                                echo '<li>
                                         <hr class="dropdown-divider">
                                     </li>';
-                                }
                             }
-                           
-                        ?>    
-                     
-                    <li>
-                        <?php
-                        if (isset($_SESSION['user'])) {
-                            ?>
-                            <a href="../../pages/admin/logout.php" class="dropdown-item"><span>Logout</span></a>
-                            <?php
                         }
+
                         ?>
-                    </li>   
+
+                        <li>
+                            <?php
+                            if (isset($_SESSION['user'])) {
+                                ?>
+                                <a href="../../pages/admin/logout.php" class="dropdown-item"><span>Logout</span></a>
+                                <?php
+                            }
+                            ?>
+                        </li>
                 </li>
-                
+                </ul>
             </ul>
-            </ul>
-            
+
 
         </div>
     </div>
